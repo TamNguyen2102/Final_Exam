e14587d58c50e9b8653a9562c2275529804f69a3
TamNguyen2102 <tamnguyen121999@gmail.com>
2021-10-26 12:18:48 +0700

    Lab4 School

---
+++ Lab_School/Lab_04/js/ex4.js
@@ -0,0 +1,33 @@
+const back = document.querySelector("#back");
+const start = document.querySelector("#start");
+const next = document.querySelector("#next");
+const img = document.querySelector("#img");
+const imgList = document.querySelector("#imageList");
+
+imgList.addEventListener("change", (e) => {
+  img.src = `./images/${e.target.value}`;
+});
+
+back.addEventListener("click", () => {
+  imgList.selectedIndex = imgList.selectedIndex - 1;
+  let imgSrc = imgList.options[imgList.selectedIndex].value;
+  img.src = `./images/${imgSrc}`;
+});
+next.addEventListener("click", () => {
+  imgList.selectedIndex = imgList.selectedIndex + 1;
+  let imgSrc = imgList.options[imgList.selectedIndex].value;
+  img.src = `./images/${imgSrc}`;
+});
+
+let stop = false;
+start.addEventListener("click", () => {
+  start.innerHTML = "Stop slideshow";
+
+  if (stop === false) {
+    const interval = setInterval(() => {
+      imgList.selectedIndex = imgList.selectedIndex + 1;
+      let imgSrc = imgList.options[imgList.selectedIndex].value;
+      img.src = `./images/${imgSrc}`;
+    }, 1500);
+  }
+});
